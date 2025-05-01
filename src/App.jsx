369f999acaeaf02c87dc1a80d3b051812f47b644
[SW5] 이아현 <laah0602@gmail.com>
2025-05-01 23:49:05 +0900

    feat: useReducer를 활용한 데이터 상태 관리

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { useReducer } from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import NewDiary from "./pages/NewDiary";
 import DiaryList from "./pages/DiaryList";
 import EditDiary from "./pages/EditDiary";
 import NotFound from "./pages/Notfound";
-// import { getEmotionImage } from "./util/get-emotion-image";
-import Button from "./components/common/Button/Button";
-import Header from "./components/layout/Header/Header";
+
+const mockData = [
+  {
+    id: 1,
+    createDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기",
+  },
+  {
+    id: 2,
+    createDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기",
+  },
+];
+
+function reducer(state, action) {
+  return state;
+}
 
 function App() {
-  //   const nav = useNavigate();
-
-  //   const onClickButton = () => {
-  //     nav("/new-diary");
-  //   };
+  const [data, dispatch] = useReducer(reducer, mockData);
 
   return (
     <>
