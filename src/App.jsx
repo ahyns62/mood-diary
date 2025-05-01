274da7a000287fb37cebbd1a8a34797d148df89a
[SW5] 이아현 <laah0602@gmail.com>
2025-05-01 23:31:41 +0900

    feat: EditDiary 페이지 라우팅 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import NewDiary from "./pages/NewDiary";
 import DiaryList from "./pages/DiaryList";
+import EditDiary from "./pages/EditDiary";
 import NotFound from "./pages/Notfound";
 // import { getEmotionImage } from "./util/get-emotion-image";
 import Button from "./components/common/Button/Button";
@@ -16,39 +17,11 @@ function App() {
 
   return (
     <>
-      <Header
-        title={"Header"}
-        leftChild={<Button text={"Left"} />}
-        rightChild={<Button text={"Right"} />}
-      />
-
-      <Button
-        text={"123"}
-        onClick={() => {
-          console.log("버튼 클릭");
-        }}
-      />
-
-      <Button
-        text={"123"}
-        type={"POSITIVE"}
-        onClick={() => {
-          console.log("버튼 클릭");
-        }}
-      />
-
-      <Button
-        text={"123"}
-        type={"NEGATIVE"}
-        onClick={() => {
-          console.log("버튼 클릭");
-        }}
-      />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new-diary" element={<NewDiary />} />
         <Route path="/list/:id" element={<DiaryList />} />
+        <Route path="/edit/:id" element={<EditDiary />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
