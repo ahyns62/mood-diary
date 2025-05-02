07a55f602f65e37fcadad3303667c507c774847c
[SW5] 이아현 <laah0602@gmail.com>
2025-05-02 23:22:27 +0900

    feat: DiaryList 컴포넌트 구현

---
+++ src/components/layout/DiaryList/DiaryList.jsx
@@ -0,0 +1,18 @@
+import Button from "../../common/Button/Button";
+
+const DiaryList = () => {
+  return (
+    <div className="DiaryList">
+      <div className="menu_bar">
+        <select>
+          <option value={"latest"}>최신순</option>
+          <option value={"oldest"}>오래된 순</option>
+        </select>
+        <Button text={"새 일기 쓰기"} />
+      </div>
+      <div className="list_wrapper">리스트</div>
+    </div>
+  );
+};
+
+export default DiaryList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,19 @@
+import Header from "../components/layout/Header/Header";
+import Button from "../components/common/Button/Button";
+import DiaryList from "../components/layout/DiaryList/DiaryList";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header
+        title={"2025-02"}
+        leftChild={<Button text={"<"} />}
+        rightChild={<Button text={">"} />}
+      />
+
+      <DiaryList />
+    </div>
+  );
 };
 
 export default Home;
