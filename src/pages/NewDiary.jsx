75698ded2df767da4a06e386c4fcc2ed98b48b81
[SW5] 이아현 <laah0602@gmail.com>
2025-05-06 16:26:35 +0900

    feat: NewDiary 페이지 레이아웃 배치

--- src/pages/NewDiary.jsx
+++ src/pages/NewDiary.jsx
@@ -1,5 +1,17 @@
+import Header from "../components/layout/Header/Header";
+import Button from "../components/common/Button/Button";
+import Editor from "../components/layout/Editor/Editor";
+
 const NewDiary = () => {
-  return <div>New Diary</div>;
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button text={"< 뒤로 가기"} />}
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default NewDiary;
