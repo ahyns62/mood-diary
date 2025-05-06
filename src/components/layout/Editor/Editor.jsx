94112747241589ac6533b843cc8561f6b718a29b
[SW5] 이아현 <laah0602@gmail.com>
2025-05-06 19:06:19 +0900

    새 일기 작성 페이지 NewDiary UI 구현

--- src/components/layout/Editor/Editor.jsx
+++ src/components/layout/Editor/Editor.jsx
@@ -1,7 +1,46 @@
 import "./Editor.css";
+import EmotionItem from "../../common/EmotionItem/EmotionItem";
+import Button from "../../common/Button/Button";
+
+const emotionList = [
+  { emotionId: 1, emotionName: "매우 좋음" },
+  { emotionId: 2, emotionName: "좋음" },
+  { emotionId: 3, emotionName: "보통" },
+  { emotionId: 4, emotionName: "나쁨" },
+  { emotionId: 5, emotionName: "매우 나쁨" },
+];
 
 const Editor = () => {
-  return <div>Editor</div>;
+  const emotionId = 2;
+
+  return (
+    <div className="Editor">
+      <section className="date_section">
+        <h3>오늘의 날짜</h3>
+        <input type="date" />
+      </section>
+      <section className="emotion_section">
+        <h3>오늘의 감정</h3>
+        <div className="emotion_list_wrapper">
+          {emotionList.map((item) => (
+            <EmotionItem
+              key={item.emotionId}
+              {...item}
+              isSelected={item.emotionId === emotionId}
+            />
+          ))}
+        </div>
+      </section>
+      <section className="content_section">
+        <h3>오늘의 일기</h3>
+        <textarea placeholder="오늘 어떤 하루를 보내셨나요?" />
+      </section>
+      <section className="button_section">
+        <Button text={"취소"} />
+        <Button text={"작성 완료"} type={"POSITIVE"} />
+      </section>
+    </div>
+  );
 };
 
 export default Editor;
