9e5e5f8d5fb37d4eae618ae1b211aa037937b2a1
[SW5] 이아현 <laah0602@gmail.com>
2025-05-08 15:27:09 +0900

    feat: EditDiary 페이지서 해당 일기 삭제 기능 구현

--- src/pages/EditDiary.jsx
+++ src/pages/EditDiary.jsx
@@ -2,16 +2,31 @@ import { useParams } from "react-router-dom";
 import Header from "../components/layout/Header/Header";
 import Button from "../components/common/Button/Button";
 import Editor from "../components/layout/Editor/Editor";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { DiaryDispatchContext } from "../App";
 
 const EditDiary = () => {
   const params = useParams();
+  const nav = useNavigate();
+  const { onDelete } = useContext(DiaryDispatchContext);
+
+  const onClickDelete = () => {
+    if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
+      onDelete(params.id);
+      nav("/", { replace: true });
+    }
+  };
 
   return (
     <div>
       <Header
         title={"일기 수정하기"}
-        rightChild={<Button text={"삭제"} type={"NEGATIVE"} />}
+        rightChild={
+          <Button onClick={onClickDelete} text={"삭제"} type={"NEGATIVE"} />
+        }
       />
+      <Editor />
     </div>
   );
 };
