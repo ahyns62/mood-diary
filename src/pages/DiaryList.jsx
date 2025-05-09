fe69488ec4aa459e4b06d19a7945d00faf62735b
[SW5] 이아현 <laah0602@gmail.com>
2025-05-09 19:57:53 +0900

    feat: DiaryList 페이지 컴포넌트 배치

--- src/pages/DiaryList.jsx
+++ src/pages/DiaryList.jsx
@@ -1,10 +1,21 @@
 import { useParams } from "react-router-dom";
+import Header from "../components/layout/Header/Header";
+import Button from "../components/common/Button/Button";
+import Viewer from "../components/layout/Viewer/Viewer";
 
 const DiaryList = () => {
   const params = useParams();
-  console.log(params);
 
-  return <div>{params.id}번 일기입니다.</div>;
+  return (
+    <div>
+      <Header
+        title={"yyyy-mm-dd 기록"}
+        leftChild={<Button text={"< 뒤로가기"} />}
+        rightChild={<Button text={"수정하기"} />}
+      />
+      <Viewer />
+    </div>
+  );
 };
 
 export default DiaryList;
