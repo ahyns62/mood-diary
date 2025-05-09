5ee019a8ef3e719822ad594bb2b00c39224160a4
[SW5] 이아현 <laah0602@gmail.com>
2025-05-09 21:24:37 +0900

    feat: useDiary 커스텀 훅 분리 및 DiaryList 기능 구현

--- src/components/layout/Editor/Editor.jsx
+++ src/components/layout/Editor/Editor.jsx
@@ -4,21 +4,7 @@ import Button from "../../common/Button/Button";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { emotionList } from "../../../util/constants";
-
-const getStringedDate = (targetDate) => {
-  let year = targetDate.getFullYear();
-  let month = targetDate.getMonth() + 1;
-  let date = targetDate.getDate();
-
-  if (month < 10) {
-    month = `0${month}`;
-  }
-  if (date < 10) {
-    date = `0${date}`;
-  }
-
-  return `${year}-${month}-${date}`;
-};
+import { getStringedDate } from "../../../util/get-stringed-date";
 
 const Editor = ({ initData, onSubmit }) => {
   const nav = useNavigate();
