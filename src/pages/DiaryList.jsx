7053b55612677e3778248c7da416accb291bf56f
[SW5] 이아현 <laah0602@gmail.com>
2025-05-09 21:08:27 +0900

    feat: DiaryList 버튼 라우팅 추가

--- src/pages/DiaryList.jsx
+++ src/pages/DiaryList.jsx
@@ -1,17 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Header from "../components/layout/Header/Header";
 import Button from "../components/common/Button/Button";
 import Viewer from "../components/layout/Viewer/Viewer";
 
 const DiaryList = () => {
   const params = useParams();
+  const nav = useNavigate();
 
   return (
     <div>
       <Header
         title={"yyyy-mm-dd 기록"}
-        leftChild={<Button text={"< 뒤로가기"} />}
-        rightChild={<Button text={"수정하기"} />}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로가기"} />}
+        rightChild={
+          <Button onClick={() => nav(`/edit/${params.id}`)} text={"수정하기"} />
+        }
       />
       <Viewer />
     </div>
